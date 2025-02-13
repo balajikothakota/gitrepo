3287f91116c84569da05d8eb8259c57f64be1401
balajikothakota <35128108+balajikothakota@users.noreply.github.com>
2025-02-13 22:09:06 +0530

    adding sysout to test2

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,6 +2,7 @@ public class TestClass {
     public void test(){
         System.out.println("test");
         //added new line
-		System.out.println("test1");
+	System.out.println("test1");
+	System.out.println("test2");
     }
 }
